62a224fb88f60d02d7ce9abfc800d753976ef119
CSIProject001 <mpxwilliams@gmail.com>
2017-01-25 13:43:22 -0800

    Add files via upload

---
+++ CS/Models/AccountViewModels/VerifyCodeViewModel.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="VerifyCodeViewModel.cs" company="Microsoft">
+//   Microsoft
+// </copyright>
+// <summary>
+//   Defines the VerifyCodeViewModel type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CS.Models.AccountViewModels
+{
+    /// <summary>
+    /// The verify code view model.
+    /// </summary>
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Remember this browser?")]
+        public bool RememberBrowser { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ CS/Models/ProductViewModels/CustomerPhonesRepository.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomerPhonesRepository.cs" company="CandiSyrup">
+//   VirtualHeights LLC
+// </copyright>
+// <summary>
+//   Defines the CustomerPhonesRepository type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CS.Models.ProductViewModels
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The customer phones repository.
+    /// </summary>
+    public class CustomerPhonesRepository: IRepository<CustomerPhone>
+    {
+        /// <summary>
+        /// Gets or sets the items.
+        /// </summary>
+        public IEnumerable<CustomerPhone> Items { get; set; }
+    }
+}
