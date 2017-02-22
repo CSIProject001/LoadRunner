6f979348772bb876a59e6d4ea18b7e6ca528c04c
CSIProject001 <purple1095>
2017-02-21 22:15:26 -0800

    AddCandiContext

---
+++ CS/Data/CandiContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS.Data
+{
+    using CS.Models.DBModels;
+    using CS.Models.OrderViewModels;
+    using CS.Models.ProductViewModels;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class CandiContext : DbContext
+    {
+        public CandiContext(DbContextOptions<CandiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>().ToTable("Address");
+            modelBuilder.Entity<Phone>().ToTable("Phone");
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Product>().ToTable("Product");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<OrderItem>().ToTable("OrderItem");
+            modelBuilder.Entity<ShoppingCart>().ToTable("ShoppingCart");
+        }
+    }
+}
