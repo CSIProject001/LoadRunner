f7b468b405ca4dd5611d274fa764eaffc08315bb
CSIProject001 <purple1095>
2017-03-13 23:42:40 -0700

    Added description to Product

--- CS/Migrations/20170314053419_Description field added to Product.Designer.cs
+++ CS/Migrations/20170314053419_Description field added to Product.Designer.cs
@@ -8,9 +8,10 @@ using CS.Data;
 namespace CS.Migrations
 {
     [DbContext(typeof(CandiContext))]
-    partial class CandiContextModelSnapshot : ModelSnapshot
+    [Migration("20170314053419_Description field added to Product")]
+    partial class DescriptionfieldaddedtoProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
---
+++ CS/Migrations/20170314053419_Description field added to Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS.Migrations
+{
+    public partial class DescriptionfieldaddedtoProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Order",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuantityPerUnit",
+                table: "Product",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Product",
+                maxLength: 100,
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "Product",
+                maxLength: 500,
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Product",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Product");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Order",
+                newName: "ID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuantityPerUnit",
+                table: "Product",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Product",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageName",
+                table: "Product",
+                nullable: true);
+        }
+    }
+}
